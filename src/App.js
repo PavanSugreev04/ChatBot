e9cfcd5da3c466dfdb60896cb26c0fa7e908f522
PavanSugreev04 <pavansugreev13@gmail.com>
2024-09-07 17:50:03 +0530

    Initial commit for AI chatbot project

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+
+import "./App.css";
+import ChatHistory from "./component/ChatHistory";
+import Loading from "./component/Loading";
+
+const App = () => {
+  const [userInput, setUserInput] = useState("");
+  const [chatHistory, setChatHistory] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+                                               // using process.env for security but the input was not taking properly should look intpo this///
+  const genAI = new GoogleGenerativeAI("AIzaSyCmX9F5wucfW9tGdpCqpURg5cdWSpoecdg");
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  const handleUserInput = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const sendMessage = async () => {
+    if (userInput.trim() === "") return;
+
+    setIsLoading(true);
+    try {
+      const result = await model.generateContent(userInput);
+      const response = await result.response;
+      console.log(response);
+      // add Gemeni's response to the chat history
+      setChatHistory([
+        ...chatHistory,
+        { type: "user", message: userInput },
+        { type: "bot", message: response.text() },
+      ]);
+    } catch {
+      console.error("Error sending message");
+    } finally {
+      setUserInput("");
+      setIsLoading(false);
+    }
+  };
+
+  const clearChat = () => {
+    setChatHistory([]);
+  };
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold text-center mb-4">AI Chatbot</h1>
+
+      <div className="chat-container rounded-lg shadow-md p-4">
+        <ChatHistory chatHistory={chatHistory} />
+        <Loading isLoading={isLoading} />
+      </div>
+
+      <div className="flex mt-4">
+        <input
+          type="text"
+          className="flex-grow px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          placeholder="Type your message..."
+          value={userInput}
+          onChange={handleUserInput}
+        />
+        <button
+          className="px-4 py-2 ml-2 rounded-lg bg-blue-500 text-white hover:bg-blue-600 focus:outline-none"
+          onClick={sendMessage}
+          disabled={isLoading}
+        >
+          Send
+        </button>
+      </div>
+      <button
+        className="mt-4 block px-4 py-2 rounded-lg bg-gray-400 text-white hover:bg-gray-500 focus:outline-none"
+        onClick={clearChat}
+      >
+        Clear Chat
+      </button>
+    </div>
+  );
+};
+
+export default App;
